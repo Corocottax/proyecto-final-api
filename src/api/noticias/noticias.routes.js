136ac95096b207c6fffcb411bf77258c2c37475c
unknown <santicorocottax@gmail.com>
2022-02-08 11:59:23 +0100

    quito la auth de getallnew

--- src/api/noticias/noticias.routes.js
+++ src/api/noticias/noticias.routes.js
@@ -4,7 +4,7 @@ const { postNewNew, getAllNew, deleteNew} = require('./noticias.controller');
 const {isUser, isAdmin}= require("../../middlewares/auth")
 
 NewsRoutes.post('/',[isAdmin], upload.single('img'), postNewNew)
-NewsRoutes.get('/', [isUser], getAllNew)
+NewsRoutes.get('/', getAllNew)
 NewsRoutes.delete('/:id', [isAdmin], deleteNew)
 
 
