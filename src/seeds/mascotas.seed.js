cdc0e5d94b466ee3908b42c7d1d545ff59dd2a20
Bertus8 <albertinisp@gmail.com>
2022-02-08 09:09:02 +0100

    seed

---
+++ src/seeds/mascotas.seed.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const mongoDb = "mongodb+srv://root:VlKxKs9jSU5t8Dnz@cluster0.te3q9.mongodb.net/Protectora?retryWrites=true&w=majority";
+const mascotaSchema = require("../api/mascota/mascota.model");
+const mascota=[
+  {
+    "nombre":"pepo",
+    "foto":"",
+    "localizacion":"",
+    "sexo":"macho",
+    "tamaño":"",
+    "especie":"cerdo vietnamita",
+    "nacimiento":"",
+    "peso":"",
+    "personalidad":"",
+    "historia":"",
+    "vacunado":true,
+    "desparasitado":true,
+    "sano":true,
+    "esterelizado":true,
+    "identificado":true,
+    "microchip":true,
+    "requisitosAdopcion":"",
+    "comentarios":"",
+    "precio":"",
+    "envio":"",
+    "estadoAdopcion":"",
+},
+
+];
+const mascotaDocuments = mascota.map((mascota) => new mascotaSchema(mascota));
+
+mongoose
+  .connect(mongoDb, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    const allMascota = await mascotaSchema.find();
+    if (allMascota.length) {
+      await mascotaSchema.collection.drop();
+    }
+  })
+  .catch((err) => console.log(`Error deleting mascotas: ${err}`))
+  .then(async () => {
+    await mascotaSchema.insertMany(mascotaDocuments);
+    console.log("mascotas successfully created");
+  })
+  .catch((err) => console.log(`Error creating mascotas: ${err}`))
+  .finally(() => mongoose.disconnect());
